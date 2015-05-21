6714752ccaecc0324cc90dbbd4fedf1720710631
ptav <web@ptavares.com>
2015-05-21 18:05:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     description='Your description',
     author='Your name',
     author_email='Your email',
-    url='http://www.python.org/sigs/distutils-sig/',
     install_requires=[],
 )
